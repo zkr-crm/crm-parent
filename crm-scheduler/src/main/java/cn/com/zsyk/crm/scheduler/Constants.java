2dbe3b9e221cb7cafb524c22169fda04563e69ae
zzh123zzh <765525107@qq.com>
2019-10-08 10:24:28 +0800

    scheduler提交

---
+++ crm-scheduler/src/main/java/cn/com/zsyk/crm/scheduler/Constants.java
@@ -0,0 +1,10 @@
+package cn.com.zsyk.crm.scheduler;
+
+public interface Constants {
+    String JOB_ID = "jobId";
+    String JOB_DESCRIPTION = "jobDescription";
+    String JOB_GROUP = "jobGroup";
+    String CRON = "cron";
+    String PARAMS = "params";
+    String TARGET_URL = "targetUrl";
+}
